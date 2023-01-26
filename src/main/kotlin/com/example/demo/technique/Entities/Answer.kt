0b6bf7d2c9c88278a238c8da05baab467c9c695f
YHDF <unes.houdaifa@gmail.com>
2023-01-26 23:29:24 +0100

    REFACTO

--- src/main/kotlin/com/example/demo/technique/Entities/Answer.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demo.technique.Entities
-import org.springframework.data.relational.core.mapping.Table
-import java.io.Serial
-
-@Table("answers")
-data class Answer (var anid:Int?, var body : String, var channel: String) {
-
-}
--- src/main/kotlin/com/example/demo/technique/Repository/AnswerRepo.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demo.technique.Repository
-
-import com.example.demo.technique.Entities.Answer
-import org.springframework.data.repository.CrudRepository
-
-interface AnswerRepo:CrudRepository<Answer,String>
